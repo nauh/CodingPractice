16194d1d9e2a5fbb5ff80734322a4ed11e63113b
nauh <nauh@users.noreply.github.com>
2015-05-05 13:18:18 -0700

    Added TreeLinkNode

---
+++ src/TreeLinkNode.java
@@ -0,0 +1,12 @@
+public class TreeLinkNode {
+
+    int val;
+    TreeLinkNode left;
+    TreeLinkNode right;
+    TreeLinkNode next;
+
+    TreeLinkNode(int val) {
+        this.val = val;
+    }
+
+}
