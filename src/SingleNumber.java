1f5e0ce45357b2126b2a0631cb2730b59b12670c
nauh <nauh@users.noreply.github.com>
2015-05-11 21:17:29 -0700

    [LeetCode] Single Number

---
+++ src/SingleNumber.java
@@ -0,0 +1,18 @@
+public class SingleNumber implements Runnable {
+
+    @Override
+    public void run() {
+    }
+
+    public int singleNumber(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
+
+}
